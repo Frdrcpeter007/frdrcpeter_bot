014ed320aa69938af8535dbf0d7ef8946f6b1a72
frdrcpeter007 <frdrcpeter@gmail.com>
2021-02-05 16:06:00 +0100

    Systeme de traduction

--- bot.js
+++ bot.js
@@ -1,4 +1,10 @@
+require('dotenv').config();
+
 const Telegram = require('node-telegram-bot-api');
+var fs = require('fs');
+var read = fs.readFileSync(__dirname + '/test.json');
+var json = JSON.parse(read);
+var translate = require('@vitalets/google-translate-api');
 
 var token = process.env.TOKEN_TELEGRAM,
     opt = {
@@ -8,8 +14,16 @@ var token = process.env.TOKEN_TELEGRAM,
 var bot = new Telegram(token, opt);
 
 bot.on('message', (msg) => {
-    console.log(msg.text)
-    if (msg.text) {
-        bot.sendMessage(msg.chat.id, `Salut @${msg.from.username} ça va ?`)
+    let text = msg.text,
+        userid = msg.from.id,
+        chatid = msg.chat.id;
+    
+
+    if (text) {
+        translate(text, { to: 'fr' }).then(textTrans => {
+            bot.sendMessage(chatid, `🈵 ${textTrans.text}`);
+        }).catch(err => {
+            bot.sendMessage(chatid, "J'ai eu du mal à traduire ton texte...")
+        });
     }
 })
